2dc3b1be744d52f2881be55c0b9592615707ab95
Carlos A Becker <caarlos0@users.noreply.github.com>
2022-10-29 21:23:16 -0300

    fix: cursor and selection
    Signed-off-by: Carlos A Becker <caarlos0@users.noreply.github.com>

--- theme.go
+++ theme.go
@@ -19,28 +19,27 @@ import (
 // valid go struct.
 // https://xtermjs.org/docs/api/terminal/interfaces/itheme/
 type Theme struct {
-	Name         string `json:"name"`
-	Background   string `json:"background"`
-	Foreground   string `json:"foreground"`
-	Selection    string `json:"selectionBackground"`
-	Cursor       string `json:"cursorColor"`
-	CursorAccent string `json:"cursorAccent"`
-	Black        string `json:"black"`
-	BrightBlack  string `json:"brightBlack"`
-	Red          string `json:"red"`
-	BrightRed    string `json:"brightRed"`
-	Green        string `json:"green"`
-	BrightGreen  string `json:"brightGreen"`
-	Yellow       string `json:"yellow"`
-	BrightYellow string `json:"brightYellow"`
-	Blue         string `json:"blue"`
-	BrightBlue   string `json:"brightBlue"`
-	Purple       string `json:"purple"`
-	BrightPurple string `json:"brightPurple"`
-	Cyan         string `json:"cyan"`
-	BrightCyan   string `json:"brightCyan"`
-	White        string `json:"white"`
-	BrightWhite  string `json:"brightWhite"`
+	Background    string `json:"background"`
+	Foreground    string `json:"foreground"`
+	Selection     string `json:"selection"`
+	Cursor        string `json:"cursor"`
+	CursorAccent  string `json:"cursorAccent"`
+	Black         string `json:"black"`
+	BrightBlack   string `json:"brightBlack"`
+	Red           string `json:"red"`
+	BrightRed     string `json:"brightRed"`
+	Green         string `json:"green"`
+	BrightGreen   string `json:"brightGreen"`
+	Yellow        string `json:"yellow"`
+	BrightYellow  string `json:"brightYellow"`
+	Blue          string `json:"blue"`
+	BrightBlue    string `json:"brightBlue"`
+	Magenta       string `json:"magenta"`
+	BrightMagenta string `json:"brightMagenta"`
+	Cyan          string `json:"cyan"`
+	BrightCyan    string `json:"brightCyan"`
+	White         string `json:"white"`
+	BrightWhite   string `json:"brightWhite"`
 }
 
 func (t Theme) String() string {
@@ -57,26 +56,26 @@ func (t Theme) String() string {
 //
 // Taken from https://github.com/meowgorithm/dotfiles.
 var DefaultTheme = Theme{
-	Background:   Background,
-	Foreground:   Foreground,
-	Cursor:       Foreground,
-	CursorAccent: Background,
-	Black:        Black,
-	BrightBlack:  BrightBlack,
-	Red:          Red,
-	BrightRed:    BrightRed,
-	Green:        Green,
-	BrightGreen:  BrightGreen,
-	Yellow:       Yellow,
-	BrightYellow: BrightYellow,
-	Blue:         Blue,
-	BrightBlue:   BrightBlue,
-	Purple:       Magenta,
-	BrightPurple: BrightMagenta,
-	Cyan:         Cyan,
-	BrightCyan:   BrightCyan,
-	White:        White,
-	BrightWhite:  BrightWhite,
+	Background:    Background,
+	Foreground:    Foreground,
+	Cursor:        Foreground,
+	CursorAccent:  Background,
+	Black:         Black,
+	BrightBlack:   BrightBlack,
+	Red:           Red,
+	BrightRed:     BrightRed,
+	Green:         Green,
+	BrightGreen:   BrightGreen,
+	Yellow:        Yellow,
+	BrightYellow:  BrightYellow,
+	Blue:          Blue,
+	BrightBlue:    BrightBlue,
+	Magenta:       Magenta,
+	BrightMagenta: BrightMagenta,
+	Cyan:          Cyan,
+	BrightCyan:    BrightCyan,
+	White:         White,
+	BrightWhite:   BrightWhite,
 }
 
 const margin = 2
--- themes.go
+++ themes.go
@@ -19,14 +19,61 @@ var (
 // Themes return the list of themes.
 var Themes = func() map[string]Theme {
 	themesOnce.Do(func() {
-		var themes []Theme
+		var themes []windowsTerminalTheme
 		if err := json.Unmarshal(themesBts, &themes); err != nil {
 			fmt.Fprintf(os.Stderr, "could not load themes.json: %s\n", err)
 			os.Exit(1)
 		}
 		for _, theme := range themes {
-			themesMap[theme.Name] = theme
+			themesMap[theme.Name] = Theme{
+				Background:    theme.Background,
+				Foreground:    theme.Foreground,
+				Selection:     theme.Selection,
+				Cursor:        theme.Cursor,
+				CursorAccent:  theme.CursorAccent,
+				Black:         theme.Black,
+				BrightBlack:   theme.BrightBlack,
+				Red:           theme.Red,
+				BrightRed:     theme.BrightRed,
+				Green:         theme.Green,
+				BrightGreen:   theme.BrightGreen,
+				Yellow:        theme.Yellow,
+				BrightYellow:  theme.BrightYellow,
+				Blue:          theme.Blue,
+				BrightBlue:    theme.BrightBlue,
+				Magenta:       theme.Purple,
+				BrightMagenta: theme.BrightPurple,
+				Cyan:          theme.Cyan,
+				BrightCyan:    theme.BrightCyan,
+				White:         theme.White,
+				BrightWhite:   theme.BrightWhite,
+			}
 		}
 	})
 	return themesMap
+}
+
+type windowsTerminalTheme struct {
+	Name         string `json:"name"`
+	Background   string `json:"background"`
+	Foreground   string `json:"foreground"`
+	Selection    string `json:"selectionBackground"`
+	Cursor       string `json:"cursorColor"`
+	CursorAccent string `json:"cursorAccent"`
+	Black        string `json:"black"`
+	BrightBlack  string `json:"brightBlack"`
+	Red          string `json:"red"`
+	BrightRed    string `json:"brightRed"`
+	Green        string `json:"green"`
+	BrightGreen  string `json:"brightGreen"`
+	Yellow       string `json:"yellow"`
+	BrightYellow string `json:"brightYellow"`
+	Blue         string `json:"blue"`
+	BrightBlue   string `json:"brightBlue"`
+	Purple       string `json:"purple"`
+	BrightPurple string `json:"brightPurple"`
+	Cyan         string `json:"cyan"`
+	BrightCyan   string `json:"brightCyan"`
+	White        string `json:"white"`
+	BrightWhite  string `json:"brightWhite"`
 }
